1b49c4966a3ad906600eac1fca76e609df44177b
haard18 <haardsolanki.itm@gmail.com>
2024-07-09 14:29:35 +0530

    chore: Add wallet.json and test.js to .gitignore

--- server/servers.js
+++ server/servers.js
@@ -2,7 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
-
+const gql = require('graphql-tag');
+const { print } = require('graphql');
 app.use(express.json());
 
 app.post('/getInfo', async (req, res) => {
@@ -157,7 +158,61 @@ app.post('/getResult', async (req, res) => {
         res.status(500).send('Error making the request');
     }
 });
-
+app.post('/getTransactions', async (req, res) => {
+    const { address } = req.body;
+  
+    // Construct the GraphQL query
+    const query = gql`
+      query {
+        transactions(
+          owners: "${address}", 
+          tags: [{ name: "Data-Protocol", values: ["ao"] }, { name: "Type", values: ["Process"] }],
+          first: 999
+        ) {
+          edges {
+            node {
+              id
+              tags {
+                name
+                value
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    try {
+      const response = await axios.post(
+        'https://arweave-search.goldsky.com/graphql',
+        {
+          query: print(query),
+        },
+        {
+          headers: {
+            'accept': 'application/graphql-response+json, application/graphql+json, application/json, text/event-stream, multipart/mixed',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/json',
+            'origin': 'https://www.ao.link',
+            'priority': 'u=1, i',
+            'referer': 'https://www.ao.link/',
+            'sec-ch-ua': '"Opera";v="111", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0'
+          }
+        }
+      );
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error making the request');
+    }
+  });
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
